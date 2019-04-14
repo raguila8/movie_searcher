52b52dd2a668e65510b44b1a92287961f62be1d2
Rodrigo <rodrigoaguilar887@gmail.com>
2019-04-13 22:29:38 -0700

    add active storage

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ require "action_view/railtie"
 require "sprockets/railtie" 
 require "rails/test_unit/railtie"
 require "action_cable/engine"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
